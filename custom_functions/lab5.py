7d133302fedda7c793ef3744d7475c2e25337afc
admin <root@localhost>
2022-05-12 12:46:19 +0000

    troubleshoot

--- custom_functions/lab5.py
+++ custom_functions/lab5.py
@@ -25,10 +25,12 @@ def lab5(current_container_id=None, peer_list_results=None, **kwargs):
     phantom.debug(peer_list_results[0][0])
     
     # add items to list
-    for item in peer_list_results[0][0]:
-        value_to_add = [ item['peer'], item['priority'], item['count'] ]
-        phantom.add_list(list_name=list_name, values=value_to_add)
+    #for item in peer_list_results[0][0]:
+    #    value_to_add = [ item['peer'], item['priority'], item['count'] ]
+    #    phantom.add_list(list_name=list_name, values=value_to_add)
     #phantom.debug(value_to_add)
+    value_to_add = [ peer_list_results[0][0][0]['peer'], peer_list_results[0][0][0]['priority'], peer_list_results[0][0][0]['count'] ]
+    phantom.add_list(list_name=list_name, values=value_to_add)
     
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
