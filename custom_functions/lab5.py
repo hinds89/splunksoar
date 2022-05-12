902e2cf118240ccbc31d5e0c8eba7c033c705fbe
admin <root@localhost>
2022-05-12 12:44:11 +0000

    troubleshoot

--- custom_functions/lab5.py
+++ custom_functions/lab5.py
@@ -25,9 +25,9 @@ def lab5(current_container_id=None, peer_list_results=None, **kwargs):
     phantom.debug(peer_list_results[0][0])
     
     # add items to list
-    #for item in peer_list_results[0][0]:
-        #value_to_add = [ item['peer'], item['priority'], item['count'] ]
-        #phantom.add_list(list_name=list_name, values=value_to_add)
+    for item in peer_list_results[0][0]:
+        value_to_add = [ item['peer'], item['priority'], item['count'] ]
+        phantom.add_list(list_name=list_name, values=value_to_add)
     #phantom.debug(value_to_add)
     
     # Return a JSON-serializable object
