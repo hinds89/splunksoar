c0848ffb600f5eb81afe420db7ee154806e727f5
admin <root@localhost>
2022-05-12 12:27:28 +0000

    custom function complete

--- custom_functions/lab5.py
+++ custom_functions/lab5.py
@@ -16,6 +16,17 @@ def lab5(current_container_id=None, peer_list_results=None, **kwargs):
     outputs = {}
     
     # Write your custom code here...
+    phantom.debug(f'current_container_id = {current_container_id}')
+    phantom.debug(f'type is = {type(current_container_id)}')
+    list_name = f'temp_{current_container_id}'
+    
+    # clear list if already exists
+    phantom.remove_list(list_name=list_name)
+    
+    # add items to list
+    for item in peer_list_results[0][0]:
+        value_to_add = [ item['peer'], item['priority'], item['count'] ]
+        phantom.add_list(list_name=list_name, values=value_to_add)
     
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
