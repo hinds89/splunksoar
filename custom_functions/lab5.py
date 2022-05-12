c80b2c2ac7d99cfba1f00b30fde19c541c3660d2
admin <root@localhost>
2022-05-12 12:59:46 +0000

    troubleshoot

--- custom_functions/lab5.py
+++ custom_functions/lab5.py
@@ -3,7 +3,7 @@ def lab5(current_container_id=None, peer_list_results=None, **kwargs):
     Custom function to parse search results from the server_peer search and store them in a new custom list.
     
     Args:
-        current_container_id (CEF type: phantom container id)
+        current_container_id (CEF type: phantom container id): Please enter the result data without the asterisk, i.e. 'run_query_1:action_result.data'
         peer_list_results (CEF type: splunk query)
     
     Returns a JSON-serializable object that implements the configured data paths:
@@ -21,16 +21,15 @@ def lab5(current_container_id=None, peer_list_results=None, **kwargs):
     list_name = f'temp_{current_container_id}'
     
     # clear list if already exists
-    # phantom.remove_list(list_name=list_name)
-    phantom.debug(peer_list_results[0][0])
+    try:
+        phantom.remove_list(list_name=list_name)
+    except:
+        phantom.debug(f'list {list_name} does not exist')
     
     # add items to list
     for item in peer_list_results[0]:
         value_to_add = [ item['peer'], item['priority'], item['count'] ]
         phantom.add_list(list_name=list_name, values=value_to_add)
-    #phantom.debug(value_to_add)
-    #value_to_add = [ peer_list_results[0][0]['peer'], peer_list_results[0][0]['priority'], peer_list_results[0][0]['count'] ]
-    #phantom.add_list(list_name=list_name, values=value_to_add)
     
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
